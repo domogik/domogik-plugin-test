fa72262600c1710f07252a26f845b5e778c11f59
Maikel Punie <maikel.punie@gmail.com>
2015-05-13 14:26:15 +0200

    check client.list.get

--- tests/010_start.py
+++ tests/010_start.py
@@ -15,5 +15,19 @@ time.sleep(10)
 cli = MQSyncReq(zmq.Context())
 msg = MQMessage()
 msg.set_action('client.list.get')
-print(cli.request('manager', msg.get(), timeout=10).get())
+req = cli.request('manager', msg.get(), timeout=10)
+
+if req:
+    data = req.get()[1]
+    found = False
+    for cli in data:
+        if cli == "plugin-test.{0}".format(get_sanitized_hostname()):
+            found = True
+            state = data[cli]["status"]
+            if state != "alive":
+                assert RuntimeError("MQ REQ client.list.get is saying the plugin is {0}".format(state))
+    if not found:
+        assert RuntimeError("MQ REQ client.list.get is not returning this plugin")
+else:
+    assert RuntimeError("MQ request to manager failed")
 
