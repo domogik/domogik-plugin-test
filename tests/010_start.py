ad400411cd445681e9ab00317b0eaaa330241fa2
Maikel Punie <maikel.punie@gmail.com>
2015-05-13 13:54:27 +0200

    added request client_list

--- tests/010_start.py
+++ tests/010_start.py
@@ -3,6 +3,14 @@
 
 from domogik.tests.common.testplugin import TestPlugin
 from domogik.common.utils import get_sanitized_hostname
+import time
 
 tp = TestPlugin('test', get_sanitized_hostname())
 tp.request_startup()
+
+time.sleep(10)
+cli = MQSyncReq(zmq.Context())
+msg = MQMessage()
+msg.set_action('client.list.get')
+print(cli.request('manager', msg.get(), timeout=10).get())
+
