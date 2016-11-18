de99284fcfa95a7caedcc63d3adfda0c6e571815
Maikel Punie <maikel.punie@gmail.com>
2016-11-18 10:15:51 +0100

    Added a lot more urls for the admin test

--- tests/080_admin.py
+++ tests/080_admin.py
@@ -2,8 +2,11 @@
 # -*- coding: utf-8 -*-
 import requests
 from domogik.common.utils import get_rest_url
+from domogik.common.utils import get_sanitized_hostname
 
+#tp = TestPlugin('test', get_sanitized_hostname())
 s = requests.session()
+cid="/client/plugin-test.{0}".format(get_sanitized_hostname())
 
 def check_url ( url, code):
     r = s.get("{0}/{1}".format(get_rest_url(True), url), allow_redirects=False)
@@ -13,21 +16,33 @@ def check_url ( url, code):
         print("URL {0} get an unexpected code exp:{1} got:{2}".format(url, code, r.status_code))
         assert RuntimeError("URL {0} got an unexpected status code exp:{1} got:{2}".format(url, code, r.status_code))
 
-print("Checking normal URLS")
+print("Checking URLS without AUTH")
 check_url( '/', 302)
 check_url( '/error', 404)
 check_url( '/login', 200)
 check_url( '/clients', 302)
+check_url( cid, 302)
+check_url( '/scenario', 302)
+check_url( '/scenario/edit/0', 302)
+check_url( '/timeline/', 302)
+check_url( '/battery/', 302)
+check_url( '/orphans', 302)
+check_url( '/users', 302)
+print("")
 
 print("Cheking rest URLS")
 check_url('/rest/', 200)
+check_url('/rest/map', 200)
 check_url('/rest/device/', 200)
 check_url('/rest/datatype', 200)
 check_url('/rest/sensor/', 200)
+print("")
 
 print("Log in")
 r = s.post("{0}/login".format(get_rest_url(True)), data={"user" : "admin", "passwd" : "123"})
+print("")
 
+print("Checking URLS that need authentication")
 check_url( '/', 200)
 check_url( '/clients', 200)
 check_url( '/scenario', 200)
@@ -36,3 +51,32 @@ check_url( '/timeline/', 200)
 check_url( '/battery/', 200)
 check_url( '/orphans', 200)
 check_url( '/users', 200)
+print("")
+
+print("Cheking client specifick URLS with authentication")
+check_url( cid, 200)
+check_url( "{0}/config".format(cid), 200)
+check_url( "{0}/dmg_devices/known".format(cid), 200)
+check_url( "{0}/dmg_devices/new".format(cid), 200)
+check_url( "{0}/dmg_devices/new/type/test.test".format(cid), 200)
+check_url( "{0}/timeline".format(cid), 200)
+check_url( "{0}/brain".format(cid), 200)
+print("")
+
+print("Log out")
+check_url( '/logout', 302)
+print("")
+
+print("Checking URLS without AUTH")
+check_url( '/', 302)
+check_url( '/error', 404)
+check_url( '/login', 200)
+check_url( '/clients', 302)
+check_url( '/scenario', 302)
+check_url( '/scenario/edit/0', 302)
+check_url( '/timeline/', 302)
+check_url( '/battery/', 302)
+check_url( '/orphans', 302)
+check_url( '/users', 302)
+print("")
+
