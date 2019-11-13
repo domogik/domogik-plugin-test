3fac9e567df26ca58f26309ecf619796c5c8218f
Nico0084 <nico84dev@gmail.com>
2019-11-13 21:45:49 +0000

    working tests

--- tests/001_config.py
+++ tests/001_config.py
@@ -3,10 +3,10 @@
 from domogik.tests.common.helpers import configure, delete_configuration, check_config
 from domogik.common.utils import get_sanitized_hostname
 
-print "==> Set the config"
+print("==> Set the config")
 delete_configuration("plugin", "test", get_sanitized_hostname())
 configure("plugin", "test", get_sanitized_hostname(), "dummy", "dummyConfigParam")
 
-print "==> Check the config"
+print("==> Check the config")
 check_config("plugin", "test", get_sanitized_hostname(), "configured", True)
 check_config("plugin", "test", get_sanitized_hostname(), "dummy", "dummyConfigParam")
--- tests/020_device.py
+++ tests/020_device.py
@@ -6,19 +6,32 @@ t = TestDevice()
 
 # an xpl device
 params = t.get_params(cid, "test.xpltest")
-params['name'] = 'Test-xpl-device'
-params['reference'] = 'Test-xpl-reference'
-params['description'] = 'Test-xpl-description'
-params['global'][0]['value'] = 'xpl-set-global'
-params['xpl'][0]['value'] = 'xpl-set-devicetype'
-params['xpl_stats']['test_xpl_stat'][0]['value'] = 'xpl-stat-set-device' 
-params['xpl_commands']['test_xpl_command'][0]['value'] = 'xpl-cmd-set-device' 
+params['name'] = 'Test-xpl-device(éèàî)'
+params['reference'] = 'Test-xpl-reference (éèàî)'
+params['description'] = 'Test-xpl-description (éèàî'
+# global params
+for the_param in params['global']:
+    if the_param['key'] == "DeviceTypeParamNoXPL":
+        the_param['value'] = 150
+    if the_param['key'] == "timer_status":
+        the_param['value'] = 10
+# xpl params
+for the_param in params['xpl']:
+    if the_param['key'] == "DeviceTypeParam":
+        the_param['value'] = 'xpl-set-devicetype (éèàî)'
+params['xpl_stats']['test_xpl_stat'][0]['value'] = 'xpl-stat-set-device (éèàî)'
+params['xpl_commands']['test_xpl_command'][0]['value'] = 200
 dev = t.create_device(params)
 
 # an mq device
 params = t.get_params(cid, "test.mqtest")
-params['name'] = 'Test-mq-device'
-params['reference'] = 'Test-mq-reference'
-params['description'] = 'Test-mq-description'
-params['global'][0]['value'] = 'mq-set-global'
+params['name'] = 'Test-mq-device (éèàî)'
+params['reference'] = 'Test-mq-reference(éèàî)'
+params['description'] = 'Test-mq-description(éèàî)'
+# global params
+for the_param in params['global']:
+    if the_param['key'] == "DeviceTypeParam":
+        the_param['value'] = 150
+    if the_param['key'] == "timer_status":
+        the_param['value'] = 10
 dev = t.create_device(params)
